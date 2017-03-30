e64f2ac59c8a70a530610ff252c183989e0515ac
xiplus <huangxuanyuxiplus@gmail.com>
2017-03-30 12:17:13 +0800

    update & add gatherarea

--- helper.js
+++ helper.js
@@ -13,24 +13,32 @@ for (var i = 0; i < height; i++) {
 			choose(el.px, el.py);
 		});
 		mymap[i][j] = [];
-		var classes = document.all["land_"+i+"_"+j].classList;
-		// 0x中立 1x我方 2x敵方
-		// 0土地 1山脈 2城堡 3主堡 9山脈or城堡
 		mymap[i][j]["type"] = 0;
-		if (classes.contains("selectable")) {
-			mymap[i][j]["type"] += 10;
-		}
-		if (classes.contains("mountain")) {
-			mymap[i][j]["type"] += 1;
-		}
-		if (classes.contains("obstacle")) {
-			mymap[i][j]["type"] += 9;
-		}
-		if (classes.contains("city")) {
-			mymap[i][j]["type"] += 2;
-		}
-		if (classes.contains("general")) {
-			mymap[i][j]["type"] += 3;
+	}
+}
+function update() {
+	for (var i = 0; i < height; i++) {
+		for (var j = 0; j < width; j++) {
+			var classes = document.all["land_"+i+"_"+j].classList;
+			// 0x中立 1x我方 2x敵方
+			// 0土地 1山脈 2城堡 3主堡 9山脈or城堡
+			mymap[i][j]["type"] = 0;
+			mymap[i][j]["army"] = document.all["land_"+i+"_"+j].innerText;
+			if (classes.contains("selectable")) {
+				mymap[i][j]["type"] += 10;
+			}
+			if (classes.contains("mountain")) {
+				mymap[i][j]["type"] += 1;
+			}
+			if (classes.contains("obstacle")) {
+				mymap[i][j]["type"] += 9;
+			}
+			if (classes.contains("city")) {
+				mymap[i][j]["type"] += 2;
+			}
+			if (classes.contains("general")) {
+				mymap[i][j]["type"] += 3;
+			}
 		}
 	}
 }
@@ -103,7 +111,12 @@ document.all["game-page"].appendChild(node);
 var node = document.createElement("div");
 node.id = "movetodiv";
 document.all.helper.appendChild(node);
-document.all.movetodiv.innerHTML = "<button style='padding: 0px 0px; margin: 0px; font-size: 18px;' onclick='movetostart();'>移動到指定地方</button>";
+document.all.movetodiv.innerHTML = "<button style='padding: 0px 0px; margin: 5px; font-size: 18px; width: 100%;' onclick='movetostart();'>移動到指定地方</button>";
+
+var node = document.createElement("div");
+node.id = "gatherdiv";
+document.all.helper.appendChild(node);
+document.all.gatherdiv.innerHTML = "<button style='padding: 0px 0px; margin: 5px; font-size: 18px; width: 100%;' onclick='gatherarea();'>聚集區域兵力</button>";
 
 var action = "";
 var actionpx, actionpy;
@@ -125,6 +138,7 @@ function movetostart() {
 }
 function moveto(x1, y1, x2, y2) {
 	console.log("move from "+x1+","+y1+" to "+x2+","+y2);
+	update();
 	var visited = [];
 	for (var i = 0; i < height; i++) {
 		visited[i] = [];
@@ -199,4 +213,77 @@ function moveto(x1, y1, x2, y2) {
 	}
 	alert("not found");
 	action = "";
+}
+function gatherarea() {
+	var selected = document.getElementsByClassName("selected")[0]
+	if (selected === undefined) {
+		console.log("choose first");
+		return;
+	}
+	if ((dis = prompt("多少步數?", "20")) !== null) {
+		px = selected.px;
+		py = selected.py;
+		console.log("gatherarea "+px+","+py+" 步數 "+dis);
+		update();
+		var visited = [];
+		for (var i = 0; i < height; i++) {
+			visited[i] = [];
+			visited[i][-1] = true;
+			for (var j = 0; j < width; j++) {
+				if ([10, 12].indexOf(mymap[i][j]["type"]) === -1) {
+					visited[i][j] = true;
+				} else {
+					visited[i][j] = false;
+				}
+			}
+			visited[i][width] = true;
+		}
+		visited[-1] = [];
+		for (var j = 0; j < width; j++) {
+			visited[-1][j] = true;
+		}
+		visited[height] = [];
+		for (var j = 0; j < width; j++) {
+			visited[height][j] = true;
+		}
+		visited[px][py] = true;
+		var queue = [];
+		var path = [];
+		var temp = {px: px, py:py};
+		queue.push(temp);
+		while (queue.length > 0 && dis > 0) {
+			var now = queue.shift();
+			if (!visited[now.px-1][now.py]) {
+				var temp = {px: now.px-1, py:now.py};
+				path.push({px: temp.px, py:temp.py, d:2});
+				queue.push(temp);
+				visited[now.px-1][now.py] = true;
+				dis--;
+			}
+			if (!visited[now.px][now.py+1]) {
+				var temp = {px: now.px, py:now.py+1};
+				path.push({px: temp.px, py:temp.py, d:3});
+				queue.push(temp);
+				visited[now.px][now.py+1] = true;
+				dis--;
+			}
+			if (!visited[now.px+1][now.py]) {
+				var temp = {px: now.px+1, py:now.py};
+				path.push({px: temp.px, py:temp.py, d:0});
+				queue.push(temp);
+				visited[now.px+1][now.py] = true;
+				dis--;
+			}
+			if (!visited[now.px][now.py-1]) {
+				var temp = {px: now.px, py:now.py-1};
+				path.push({px: temp.px, py:temp.py, d:1});
+				queue.push(temp);
+				visited[now.px][now.py-1] = true;
+				dis--;
+			}
+		}
+		for (var i = path.length - 1; i >= 0; i--) {
+			move(path[i].px, path[i].py, path[i].d);
+		}
+	}
 }
