65ebf897d0d927a8bb47f63d7e0b577ef6a44b7f
xiplus <huangxuanyuxiplus@gmail.com>
2017-03-30 12:49:08 +0800

    add load bookmarklet

---
+++ load.js
@@ -0,0 +1,7 @@
+javascript: (function(){
+	var head = document.getElementsByTagName('head')[0];
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = "http://xiplus.twbbs.org/generals.io-helper/helper.js";
+	head.appendChild(script);
+})();
